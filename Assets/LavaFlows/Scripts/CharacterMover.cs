0225b8f356db53de31ea83eeb54b942ee2531080
TapAlexxx <lordpictures123@gmail.com>
2025-10-21 23:30:52 +0300

    movement + animation fixes

--- Assets/LavaFlows/Scripts/CharacterMover.cs
+++ Assets/LavaFlows/Scripts/CharacterMover.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class CharacterMover : MonoBehaviour
 {
     [SerializeField] private Transform character;
-    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float forwardSpeed = 6f;
+    [SerializeField] private float backwardSpeed = 3f;
+    [SerializeField] private float sideSpeed = 5f;
     [SerializeField] private Animator animator;
 
     private Vector2 moveDirection;
@@ -12,15 +14,17 @@ public class CharacterMover : MonoBehaviour
     {
         moveDirection.x = Input.GetAxis("Vertical");
         moveDirection.y = Input.GetAxis("Horizontal");
-        
-        Move();
+
+        var speedX = moveDirection.x * (moveDirection.x >= 0 ? forwardSpeed : backwardSpeed);
+        var speedY = moveDirection.y * sideSpeed;
+        Move(speedX, speedY);
         
         animator.SetFloat("moveDirectionX", moveDirection.x);
         animator.SetFloat("moveDirectionY", moveDirection.y);
     }
 
-    private void Move()
+    private void Move(float speedX, float speedY)
     {
-        transform.position += new Vector3(moveDirection.y * moveSpeed * Time.deltaTime, 0, moveDirection.x * moveSpeed * Time.deltaTime);
+        transform.position += new Vector3(speedY * Time.deltaTime, 0, speedX * Time.deltaTime);
     }
 }
